cf0f61c7858244c967fc74690702c0d118a1ded4
Viktoria <toz.vito@gmail.com>
2025-07-12 21:53:36 +0200

    feat: adds some js, modifies the foto

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ fetch('img/led-example.svg')
   .then(svgText => {
     document.querySelector('.preloader-ex').innerHTML = svgText;
   });
+
+const navItems = document.querySelectorAll('.nav__item');
+
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navItems.forEach(el => el.classList.remove('nav__item--active'));
+    item.classList.add('nav__item--active');
+  });
+});
